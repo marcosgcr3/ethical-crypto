cd7feb5eba1fa398da1fca48c2860206f0d269de
marcosgcr3 <marcosgcr3@gmail.com>
2026-04-23 14:12:48 +0200

    Update Formspree ID to mbdqpnvz

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,7 +3,7 @@
 import { useForm, ValidationError } from '@formspree/react';
 
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm('myklbrek');
+  const [state, handleSubmit] = useForm('mbdqpnvz');
 
   if (state.succeeded) {
     return (
